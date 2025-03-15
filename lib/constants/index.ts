ae79837ec1cffab7183d321275882f15a6973329
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2025-03-15 11:36:35 +0530

    chore: added user authentication using supabase

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const protectedPaths = [ "/", "/chat",
+    "/dashboard",
+    ];
+export const authPaths = ["/register", "/signin"];
+
+export const departments = ["CSE", "ISE", "ECE", "EEE", "ME", "CV", "CH", "BT", "IEM", "EIE", "ETE", "AIML", "ASE"]
